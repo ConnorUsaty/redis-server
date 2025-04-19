d4258898e1ca5f4cf4ed3d852b746991d6639ea5
ConnorUsaty <connorusaty@gmail.com>
2025-04-18 22:53:14 -0400

    feat: main loop + pseudo code

---
+++ server.cpp
@@ -0,0 +1,17 @@
+
+
+int main() {
+    // get socket (allocates a socket and gives us an fd)
+    // bind socket to a port
+    // set port to be listening (server), rather then connecting (client)
+
+    // while(1){
+        // accept incoming connections and get connection fd
+        // error handling
+        // send thread to deal with connection fd if valid
+    // }
+
+    // close server fd (server socket)
+
+    return 0;
+}
