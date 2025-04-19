a3fa82d41e9cf4525e2f7e23dd35162c4ed179c4
ConnorUsaty <connorusaty@gmail.com>
2025-04-18 22:58:21 -0400

    feat: main loop + pseudo code

---
+++ client.cpp
@@ -0,0 +1,9 @@
+
+int main() {
+    // get socket (allocate socket and get client socket fd)
+    // set as connector socket (client) and initiate connection with server by putting in same same address (port) as server
+
+    // close client socket fd 
+
+    return 0;
+}
