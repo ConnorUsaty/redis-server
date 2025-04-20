a4b69f21bc15ca6f78a586f21401057fa1407031
Connor Usaty <connorusaty@gmail.com>
2025-04-19 21:34:10 -0400

    feat: socket config + server connection

--- client.cpp
+++ client.cpp
@@ -1,9 +1,26 @@
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
 
 int main() {
     // get socket (allocate socket and get client socket fd)
-    // set as connector socket (client) and initiate connection with server by putting in same same address (port) as server
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // close client socket fd 
+    // set socket adresses
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);        // port
+    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0
+
+    // set as connector socket (client) and initiate connection with server by putting in same same address (port) as server
+    if(connect(client_fd, (const struct sockaddr *)&addr, sizeof(addr)) != 0){
+        std::cerr << "Error connecting to server\n";
+        return 1;
+    }
+
+    // close client socket 
+    close(client_fd);
 
     return 0;
 }
