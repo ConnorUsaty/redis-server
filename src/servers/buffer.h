39bb246af1aa1bec34deab1358aae6ecbe1abfc8
ConnorUsaty <connorusaty@gmail.com>
2025-07-05 17:41:10 -0700

    perf: optimize Buffer allocations and alignment

--- src/servers/buffer.h
+++ src/servers/buffer.h
@@ -2,36 +2,45 @@
 
 #include <cassert>
 #include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 /* Buffer structure optimized for our exact use case
  * Allows for more efficient consumes and clears than std::vector */
 
-struct Buffer {
+struct alignas(64) Buffer {
   uint8_t* buf_start_;
   uint8_t* buf_end_;
   uint8_t* data_start_;
   uint8_t* data_end_;
 
   Buffer(size_t sz) {
-    buf_start_ = new uint8_t[sz];
+    // round to nearest multiple of 64
+    sz = (sz + 63U) & ~63U;
+
+    buf_start_ = static_cast<uint8_t*>(std::aligned_alloc(64, sz));
     buf_end_ = buf_start_ + sz;
     data_start_ = buf_start_;
     data_end_ = buf_start_;
   }
 
-  ~Buffer() { delete[] buf_start_; }
+  ~Buffer() { std::free(buf_start_); }
 
   Buffer(const Buffer&) = delete;
   Buffer& operator=(const Buffer&) = delete;
   Buffer(Buffer&&) = delete;
   Buffer& operator=(Buffer&&) = delete;
 
-  size_t size() const { return static_cast<size_t>(data_end_ - data_start_); }
+  inline size_t size() const noexcept {
+    return static_cast<size_t>(data_end_ - data_start_);
+  }
 
-  size_t capacity() const { return static_cast<size_t>(buf_end_ - buf_start_); }
+  inline size_t capacity() const noexcept {
+    return static_cast<size_t>(buf_end_ - buf_start_);
+  }
 
-  uint8_t* data() const { return data_start_; }
+  inline uint8_t* data() const noexcept { return data_start_; }
 
   // for debugging purposes
   void print_data() const {
@@ -44,7 +53,7 @@ struct Buffer {
     std::cout << "}\n";
   }
 
-  void clear() { data_start_ = data_end_ = buf_start_; }
+  inline void clear() noexcept { data_start_ = data_end_ = buf_start_; }
 
   void consume(size_t sz) {
     assert(sz <= size());
@@ -66,7 +75,7 @@ struct Buffer {
       // we can get more room by either ensuring data_start_ = buf_start_
       // which will give us (data_start_ - buf_start_) more bytes
       // or we can entirely re-allocate a larger buffer
-      if (avail_back + avail_front < total_req) {
+      if (avail_back + avail_front < total_req) [[unlikely]] {
         // just re-allocate
         size_t buf_sz = buf_end_ - buf_start_;
         size_t data_sz = data_end_ - data_start_;
@@ -74,9 +83,10 @@ struct Buffer {
           buf_sz <<= 1;
         }
 
-        uint8_t* t = new uint8_t[buf_sz];
+        // buf_sz is already guarenteed to be a multiple of 64
+        uint8_t* t = static_cast<uint8_t*>(std::aligned_alloc(64, buf_sz));
         memcpy(t, data_start_, data_sz);
-        delete[] buf_start_;
+        std::free(buf_start_);
 
         buf_start_ = t;
         buf_end_ = buf_start_ + buf_sz;
